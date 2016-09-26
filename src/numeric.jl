8e71df587aa676fb724551f7c19cd8a14a79fdca
Fengyang Wang <fengyang.wang.0@gmail.com>
2016-09-26 12:51:52 -0400

    Update LICENSE to MIT

--- src/numeric.jl
+++ src/numeric.jl
@@ -1,15 +1,9 @@
-#= Copyright 2015 Fengyang Wang
-
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-this file except in compliance with the License. You may obtain a copy of the
-License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software distributed
-under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-CONDITIONS OF ANY KIND, either express or implied. See the License for the
-specific language governing permissions and limitations under the License. =#
+#=――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+    NOTE
+    The algorithm used here only works up to vigintillions. Beyond that, it is
+    difficult to find consensus on how numbers should be displayed. It is also
+    of dubious real-world utility.
+――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――=#
 
 @reexport module Numeric
 
--- src/pluralize.jl
+++ src/pluralize.jl
@@ -1,16 +1,4 @@
-#= Copyright 2016 Fengyang Wang
-
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-this file except in compliance with the License. You may obtain a copy of the
-License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software distributed
-under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-CONDITIONS OF ANY KIND, either express or implied. See the License for the
-specific language governing permissions and limitations under the License.
-――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+#=――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
     NOTE
     The algorithm used for inflecting English words into plurals is descibed by
     Damian Conway of Monash University. The original paper is accessible at
