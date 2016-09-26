6c3128818afb598eb7dd6c2c1b4d129b3eb13d41
Fengyang Wang <fengyang.wang.0@gmail.com>
2016-09-26 12:54:16 -0400

    Update test LICENSE and REQUIRE

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,6 @@
-#= Copyright 2015 Fengyang Wang
-
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-this file except in compliance with the License. You may obtain a copy of the
-License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software distributed
-under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-CONDITIONS OF ANY KIND, either express or implied. See the License for the
-specific language governing permissions and limitations under the License. =#
-
 using English
 
-if VERSION ≥ v"0.5.0-dev+7720"
-    using Base.Test
-else
-    using BaseTestNext
-    Test = BaseTestNext
-end
+using Base.Test
 
 @testset "Articles" begin
     @test indefinite("pig") == "a"
