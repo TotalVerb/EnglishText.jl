7b8150ab814e33a54ee262fcba7a529af9b48922
autodocs <autodocs>
2018-01-30 06:39:08 +0000

    build based on 8f668ba

--- latest/search_index.js
+++ latest/search_index.js
@@ -21,7 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "EnglishText.jl Documentation",
     "title": "EnglishText.Articulate.indefinite",
     "category": "Function",
-    "text": "indefinite(word)\n\nDetermine the correct indefinite article, from “a” or “an”, for the given noun.\n\njulia> using EnglishText\n\njulia> indefinite(\"hour\")\n\"an\"\n\njulia> indefinite(\"hand\")\n\"a\"\n\n\n\n"
+    "text": "indefinite(word)\n\nDetermine the correct indefinite article, from “a” or “an”, for the given noun.\n\njulia> using EnglishText\n\njulia> indefinite(\"hour\")\n\"an\"\n\njulia> indefinite(\"hand\")\n\"a\"\n\n\n\n\n\n"
 },
 
 {
@@ -37,7 +37,7 @@ var documenterSearchIndex = {"docs": [
     "page": "EnglishText.jl Documentation",
     "title": "EnglishText.Numeric.english",
     "category": "Function",
-    "text": "english(n::Integer)\n\nConvert n to English, given that 0 le n  10^66.\n\njulia> using EnglishText\n\njulia> english(16)\n\"sixteen\"\n\n\n\n"
+    "text": "english(n::Integer)\n\nConvert n to English, given that 0 le n  10^66.\n\njulia> using EnglishText\n\njulia> english(16)\n\"sixteen\"\n\n\n\n\n\n"
 },
 
 {
@@ -45,7 +45,7 @@ var documenterSearchIndex = {"docs": [
     "page": "EnglishText.jl Documentation",
     "title": "EnglishText.Numeric.unenglish",
     "category": "Function",
-    "text": "unenglish(T <: Integer, data::AbstractString) → T\n\nConvert data to an integral type. This function has the guarantee that unenglish(Int, english(x)) == x, modulo any type differences. It is not guaranteed to work well or throw exceptions on other inputs.\n\njulia> using EnglishText\n\njulia> unenglish(Int, \"sixteen\")\n16\n\n\n\n"
+    "text": "unenglish(T <: Integer, data::AbstractString) → T\n\nConvert data to an integral type. This function has the guarantee that unenglish(Int, english(x)) == x, modulo any type differences. It is not guaranteed to work well or throw exceptions on other inputs.\n\njulia> using EnglishText\n\njulia> unenglish(Int, \"sixteen\")\n16\n\n\n\n\n\n"
 },
 
 {
@@ -61,23 +61,23 @@ var documenterSearchIndex = {"docs": [
     "page": "EnglishText.jl Documentation",
     "title": "EnglishText.Quantities.ItemQuantity",
     "category": "Type",
-    "text": "ItemQuantity(n::Integer, item::AbstractString)\n\nRepresents a quantity of n occurrences of item. Although this is not a collection, for ease of use, it implements some of the standard collection methods length (for number of items) and isempty (for whether there are no items).\n\njulia> using EnglishText\n\njulia> ItemQuantity(2, \"apple\")\n2 apples\n\njulia> ItemQuantity(1, \"standard canine\")\n1 standard canine\n\n\n\n"
+    "text": "ItemQuantity(n::Integer, item::AbstractString)\n\nRepresents a quantity of n occurrences of item. Although this is not a collection, for ease of use, it implements some of the standard collection methods length (for number of items) and isempty (for whether there are no items).\n\njulia> using EnglishText\n\njulia> ItemQuantity(2, \"apple\")\n2 apples\n\njulia> ItemQuantity(1, \"standard canine\")\n1 standard canine\n\n\n\n\n\n"
 },
 
 {
-    "location": "index.html#Base.isempty-Tuple{EnglishText.Quantities.ItemQuantity}",
+    "location": "index.html#Base.isempty-Tuple{ItemQuantity}",
     "page": "EnglishText.jl Documentation",
     "title": "Base.isempty",
     "category": "Method",
-    "text": "isempty(quantity::ItemQuantity)\n\nReturn true if the given ItemQuantity represents no items.\n\njulia> using EnglishText\n\njulia> isempty(ItemQuantity(0, \"orange\"))\ntrue\n\njulia> isempty(ItemQuantity(4, \"person\"))\nfalse\n\n\n\n"
+    "text": "isempty(quantity::ItemQuantity)\n\nReturn true if the given ItemQuantity represents no items.\n\njulia> using EnglishText\n\njulia> isempty(ItemQuantity(0, \"orange\"))\ntrue\n\njulia> isempty(ItemQuantity(4, \"person\"))\nfalse\n\n\n\n\n\n"
 },
 
 {
-    "location": "index.html#Base.length-Tuple{EnglishText.Quantities.ItemQuantity}",
+    "location": "index.html#Base.length-Tuple{ItemQuantity}",
     "page": "EnglishText.jl Documentation",
     "title": "Base.length",
     "category": "Method",
-    "text": "length(quantity::ItemQuantity)\n\nReturn the number of items represented by this quantity.\n\njulia> using EnglishText\n\njulia> length(ItemQuantity(7, \"desk\"))\n7\n\n\n\n"
+    "text": "length(quantity::ItemQuantity)\n\nReturn the number of items represented by this quantity.\n\njulia> using EnglishText\n\njulia> length(ItemQuantity(7, \"desk\"))\n7\n\n\n\n\n\n"
 },
 
 {
@@ -85,7 +85,7 @@ var documenterSearchIndex = {"docs": [
     "page": "EnglishText.jl Documentation",
     "title": "EnglishText.Pluralize.pluralize",
     "category": "Function",
-    "text": "pluralize(word; classical=true)\n\nPluralize a singular noun word (given in canonical capitalization) using heuristics and lists of exceptions. If word is not a singular noun, this function may give strange results.\n\nIf classical is set to true, then the classical (i.e. inherited from Latin or Greek) pluralization is chosen instead of the anglicized pluralization. As an example, the classical plural of \"vertex\" is \"vertices\", but the anglicized plural is \"vertexes\". By default, the classical pluralization is used.\n\njulia> using EnglishText\n\njulia> pluralize(\"fox\")\n\"foxes\"\n\njulia> pluralize(\"radius\")\n\"radii\"\n\njulia> pluralize(\"radius\", classical=false)\n\"radiuses\"\n\n\n\n"
+    "text": "pluralize(word; classical=true)\n\nPluralize a singular noun word (given in canonical capitalization) using heuristics and lists of exceptions. If word is not a singular noun, this function may give strange results.\n\nIf classical is set to true, then the classical (i.e. inherited from Latin or Greek) pluralization is chosen instead of the anglicized pluralization. As an example, the classical plural of \"vertex\" is \"vertices\", but the anglicized plural is \"vertexes\". By default, the classical pluralization is used.\n\njulia> using EnglishText\n\njulia> pluralize(\"fox\")\n\"foxes\"\n\njulia> pluralize(\"radius\")\n\"radii\"\n\njulia> pluralize(\"radius\", classical=false)\n\"radiuses\"\n\n\n\n\n\n"
 },
 
 {
@@ -93,7 +93,7 @@ var documenterSearchIndex = {"docs": [
     "page": "EnglishText.jl Documentation",
     "title": "EnglishText.Pluralize.singularize",
     "category": "Function",
-    "text": "singularize(word)\n\nUnpluralize a plural noun (given in canonical capitalization) using heuristics and lists of exceptions. If the given word is not a plural noun, the result may be unpredictable.\n\njulia> using EnglishText\n\njulia> singularize(\"foxes\")\n\"fox\"\n\njulia> singularize(\"data\")\n\"datum\"\n\n\n\n"
+    "text": "singularize(word)\n\nUnpluralize a plural noun (given in canonical capitalization) using heuristics and lists of exceptions. If the given word is not a plural noun, the result may be unpredictable.\n\njulia> using EnglishText\n\njulia> singularize(\"foxes\")\n\"fox\"\n\njulia> singularize(\"data\")\n\"datum\"\n\n\n\n\n\n"
 },
 
 {
@@ -109,7 +109,7 @@ var documenterSearchIndex = {"docs": [
     "page": "EnglishText.jl Documentation",
     "title": "EnglishText.ItemLists.ItemList",
     "category": "Type",
-    "text": "ItemList(objects, connective=Sum())\n\nA list of items or adjectives, which supports printing in standard English format. The first argument objects should be an iterator over some number of strings or other objects, including EnglishText.ItemQuantity objects.\n\nThe second argument connective should be one of:\n\nSum(), which represents a list of nouns in a collection of things\nDisjunction(), which represents a list of traits (typically adjectives or adverbs, but possibly also verbs or nouns) for which at least one should be satisfied\nConjunction(), which represents a list of traits that should all be satisfied\n\nIf omitted, connective is set to Sum().\n\njulia> using EnglishText\n\njulia> ItemList([\"apples\", \"oranges\"])\napples and oranges\n\njulia> ItemList([ItemQuantity(2, \"pencil\"), ItemQuantity(1, \"pen\")])\n2 pencils and 1 pen\n\njulia> ItemList([\"animal\", \"plant\"], Disjunction())\nanimal or plant\n\njulia> ItemList([\"red\", \"blue\", \"white\"], Conjunction())\nred, blue, and white\n\njulia> \"Help us use and test this software.\"\n\"Help us use and test this software.\"\n\n\n\n"
+    "text": "ItemList(objects, connective=Sum())\n\nA list of items or adjectives, which supports printing in standard English format. The first argument objects should be an iterator over some number of strings or other objects, including EnglishText.ItemQuantity objects.\n\nThe second argument connective should be one of:\n\nSum(), which represents a list of nouns in a collection of things\nDisjunction(), which represents a list of traits (typically adjectives or adverbs, but possibly also verbs or nouns) for which at least one should be satisfied\nConjunction(), which represents a list of traits that should all be satisfied\n\nIf omitted, connective is set to Sum().\n\njulia> using EnglishText\n\njulia> ItemList([\"apples\", \"oranges\"])\napples and oranges\n\njulia> ItemList([ItemQuantity(2, \"pencil\"), ItemQuantity(1, \"pen\")])\n2 pencils and 1 pen\n\njulia> ItemList([\"animal\", \"plant\"], Disjunction())\nanimal or plant\n\njulia> ItemList([\"red\", \"blue\", \"white\"], Conjunction())\nred, blue, and white\n\njulia> \"Help us use and test this software.\"\n\"Help us use and test this software.\"\n\n\n\n\n\n"
 },
 
 {
@@ -125,7 +125,7 @@ var documenterSearchIndex = {"docs": [
     "page": "EnglishText.jl Documentation",
     "title": "EnglishText.Text.sentences",
     "category": "Function",
-    "text": "sentences(text::AbstractString)\n\nReturn an iterable over the Sentences contained within text. Sentences are identified naïvely; that is, every full stop, exclamation mark, or question mark is considered to delimit a sentence. This is of course prone to error, as some full stops are used for abbreviations and not for delimiting sentences.\n\njulia> using EnglishText\n\njulia> for s in sentences(\"Hi! Iterate over sentences. OK?\")\n           println(s)\n       end\nHi!\nIterate over sentences.\nOK?\n\n\n\n"
+    "text": "sentences(text::AbstractString)\n\nReturn an iterable over the Sentences contained within text. Sentences are identified naïvely; that is, every full stop, exclamation mark, or question mark is considered to delimit a sentence. This is of course prone to error, as some full stops are used for abbreviations and not for delimiting sentences.\n\njulia> using EnglishText\n\njulia> for s in sentences(\"Hi! Iterate over sentences. OK?\")\n           println(s)\n       end\nHi!\nIterate over sentences.\nOK?\n\n\n\n\n\n"
 },
 
 {
@@ -141,7 +141,7 @@ var documenterSearchIndex = {"docs": [
     "page": "EnglishText.jl Documentation",
     "title": "EnglishText.Semantics.SemanticText",
     "category": "Type",
-    "text": "An object representing a string of text but with additional semantic information. These objects convert to Strings through the string function, but also typically support other operations.\n\n\n\n"
+    "text": "An object representing a string of text but with additional semantic information. These objects convert to Strings through the string function, but also typically support other operations.\n\n\n\n\n\n"
 },
 
 {
